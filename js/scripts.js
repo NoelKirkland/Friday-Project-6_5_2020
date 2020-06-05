55d7e2542bf5f1ca0d16c872da93d57124f32ed1
Noel Kirkland <noelkirkland@gmail.com>
2020-06-05 10:56:24 -0700

    add the very first branch of logic to the scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,8 @@ $(document).ready(function(){
     $("#front-showing").hide();
   });
   $("form#info").submit(function(event){
-    const availability = parseInt($("input#hours-available").val());
     const pref = $("input:radio[name=preference]:checked").val();
+    const availability = parseInt($("input#hours-available").val());
     const personality = $("#personality-type").val();
     if(availability <= 0 || availability > 24){
       alert("Please enter a numnber between 1 and 24");
@@ -18,8 +18,12 @@ $(document).ready(function(){
     else if(!availability){
       alert("Please input only numbers");
     }
+    else{
 
+      if(pref === "front-end" && (availability >=1 || availability <=4) && (personality === "Extroverted & Passive")){
+        $("#match").text("HTML");
       }
+
       $("#answer").show();
     }
     event.preventDefault();
