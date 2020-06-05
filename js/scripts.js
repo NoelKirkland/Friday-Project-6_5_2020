dde5b55a2fce79b11ebdc1da3dbcda080931dd9e
Noel Kirkland <noelkirkland@gmail.com>
2020-06-05 10:14:00 -0700

    add three input methods to the page; a form submission section, radio buttons, and a check-box drop down menue. I also began to write the JS logic. I made sure to sart my branch with a few alerts to make sure the user is entering the correct type of data.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,27 @@
 
 $(document).ready(function(){
-  $(".front-end").click(function() {
-    $("#front-showing").fadeIn();
+  $("#front-end").click(function() {
+    $("#front-showing").fadeToggle();
     $("#back-showing").hide();
   });
-  $(".back-end").click(function() {
-    $("#back-showing").fadeIn();
+  $("#back-end").click(function() {
+    $("#back-showing").fadeToggle();
     $("#front-showing").hide();
   });
+  $("form#info").submit(function(event){
+    const availability = parseInt($("input#hours-available").val());
+    const pref = $("input:radio[name=preference]:checked").val();
+    const personality = $("#personality-type").val();
+    if(availability <= 0 || availability > 24){
+      alert("Please enter a numnber between 1 and 24");
+    }
+    else if(!availability){
+      alert("Please input only numbers");
+    }
 
+      }
+      $("#answer").show();
+    }
     event.preventDefault();
   });
 });
