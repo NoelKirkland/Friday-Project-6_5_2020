2b02e688a72d4e3518541d9d1f53568e2ff15845
Noel Kirkland <noelkirkland@gmail.com>
2020-06-05 11:43:43 -0700

    complete all possible branches

--- js/scripts.js
+++ js/scripts.js
@@ -44,6 +44,31 @@ $(document).ready(function(){
       else if(pref === "front-end" && (availability >12) && (personality === "Introverted & Passive" || personality === "Introverted & Aggressive")){
         $("#match").text("JavaScript, JQuery, and React.js");
       }
+      // this is the end of all "front-end" selections and the begining of all of the "back-end" selections
+      else if(pref === "back-end" && (availability >=1 && availability <=6) && (personality === "Extroverted & Passive")){
+        $("#match").text("PHP");
+      }
+      else if(pref === "back-end" && (availability >=1 && availability <=6) && (personality === "Extroverted & Aggressive")){
+        $("#match").text("C++");
+      }
+      else if(pref === "back-end" && (availability >=1 && availability <=6) && (personality === "Introverted & Passive" || personality === "Introverted & Aggressive")){
+        $("#match").text("Java");
+      }
+      else if(pref === "back-end" && (availability >=7 && availability <=12) && (personality === "Extroverted & Passive")){
+        $("#match").text("PHP as well C++");
+      }
+      else if(pref === "back-end" && (availability >=7 && availability <=12) && (personality === "Extroverted & Aggressive")){
+        $("#match").text("C++ as well as Python");
+      }
+      else if(pref === "back-end" && (availability >=7 && availability <=12) && (personality === "Introverted & Passive" || personality === "Introverted & Aggressive")){
+        $("#match").text("Java as well as Node.js");
+      }
+      else if(pref === "back-end" && (availability >12) && (personality === "Extroverted & Passive" || personality === "Extroverted & Aggressive")){
+        $("#match").text("PHP, C++, and Python");
+      }
+      else if(pref === "back-end" && (availability >12) && (personality === "Introverted & Passive" || personality === "Introverted & Aggressive")){
+        $("#match").text("Java, Node.js, and Ruby");
+      }
       else{
         $("#match").text("Doesn't fit and current chriteria");
       }
