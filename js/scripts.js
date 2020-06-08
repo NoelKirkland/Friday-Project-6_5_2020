25dc04bbfc5f6e3937e573ab7e3b073dccf4e6d8
Noel Kirkland <noelkirkland@gmail.com>
2020-06-07 19:38:13 -0700

    fix bug where id match was just showing up once. Of course I had to change it to a class to use it multiple times. Plus, updated styling a little bit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-
+// Business logic:
 function multiply(hours, days) {
   return hours * days
 }
 function devide(mastery, resulta){
   return mastery / resulta
 }
-
+// User interface:
 $(document).ready(function(){
   $("#front-end").click(function() {
     $("#front-showing").fadeToggle();
@@ -39,62 +39,62 @@ $(document).ready(function(){
       }
       else if(!days || days <= 0 || days > 7){
         alert("Please enter your correct days-per-week availability")
-        if(pref === "front-end" && (exp === "None at all") && (personality === "Extroverted & Passive")){
-          $("#match").text("HTML");
-        }
-        else if(pref === "front-end" && (exp === "None at all") && (personality === "Extroverted & Aggressive")){
-          $("#match").text("CSS");
-        }
-        else if(pref === "front-end" && (exp === "None at all") && (personality === "Introverted & Passive" || personality === "Introverted & Aggressive")){
-          $("#match").text("JavaScript");
-        }
-        else if(pref === "front-end" && (exp === "A little bit") && (personality === "Extroverted & Passive")){
-          $("#match").text("HTML as well as CSS");
-        }
-        else if(pref === "front-end" && (exp === "A little bit") && (personality === "Extroverted & Aggressive")){
-          $("#match").text("CSS as well as Bootstrap");
-        }
-        else if(pref === "front-end" && (exp === "A little bit") && (personality === "Introverted & Passive" || personality === "Introverted & Aggressive")){
-          $("#match").text("JavaScript as well as JQuery");
-        }
-        else if(pref === "front-end" && (exp === "A moderate amount") && (personality === "Extroverted & Passive" || personality === "Extroverted & Aggressive")){
-          $("#match").text("HTML, CSS, and Bootstrap");
-        }
-        else if(pref === "front-end" && (exp === "A moderate amount") && (personality === "Introverted & Passive" || personality === "Introverted & Aggressive")){
-          $("#match").text("JavaScript, JQuery, and React.js");
-        }
-        else if(pref === "back-end" && (exp === "None at all") && (personality === "Extroverted & Passive")){
-          $("#match").text("PHP");
-        }
-        else if(pref === "back-end" && (exp === "None at all") && (personality === "Extroverted & Aggressive")){
-          $("#match").text("C++");
-        }
-        else if(pref === "back-end" && (exp === "None at all") && (personality === "Introverted & Passive" || personality === "Introverted & Aggressive")){
-          $("#match").text("Java");
-        }
-        else if(pref === "back-end" && (exp === "A little bit") && (personality === "Extroverted & Passive")){
-          $("#match").text("PHP as well C++");
-        }
-        else if(pref === "back-end" && (exp === "A little bit") && (personality === "Extroverted & Aggressive")){
-          $("#match").text("C++ as well as Python");
-        }
-        else if(pref === "back-end" && (exp === "A little bit") && (personality === "Introverted & Passive" || personality === "Introverted & Aggressive")){
-          $("#match").text("Java as well as Node.js");
-        }
-        else if(pref === "back-end" && (exp === "A moderate amount") && (personality === "Extroverted & Passive" || personality === "Extroverted & Aggressive")){
-          $("#match").text("PHP, C++, and Python");
-        }
-        else if(pref === "back-end" && (exp === "A moderate amount") && (personality === "Introverted & Passive" || personality === "Introverted & Aggressive")){
-          $("#match").text("Java, Node.js, and Ruby");
-        }
-        else{
-          $("#match").text("Doesn't fit and current chriteria");
-        }
-      }  
-        $("#hours-show").text(hours)
-        $("#days-show").text(days)
-        $("#output").text(resultb.toFixed()); 
-        $("#answer").show();
+      }
+      else if(pref === "front-end" && (exp === "None at all") && (personality === "Extroverted & Passive")){
+        $(".match").text("HTML");
+      }
+      else if(pref === "front-end" && (exp === "None at all") && (personality === "Extroverted & Aggressive")){
+        $(".match").text("CSS");
+      }
+      else if(pref === "front-end" && (exp === "None at all") && (personality === "Introverted & Passive" || personality === "Introverted & Aggressive")){
+        $(".match").text("JavaScript");
+      }
+      else if(pref === "front-end" && (exp === "A little bit") && (personality === "Extroverted & Passive")){
+        $(".match").text("HTML and CSS");
+      }
+      else if(pref === "front-end" && (exp === "A little bit") && (personality === "Extroverted & Aggressive")){
+        $(".match").text("CSS and Bootstrap");
+      }
+      else if(pref === "front-end" && (exp === "A little bit") && (personality === "Introverted & Passive" || personality === "Introverted & Aggressive")){
+        $(".match").text("JavaScript and JQuery");
+      }
+      else if(pref === "front-end" && (exp === "A moderate amount") && (personality === "Extroverted & Passive" || personality === "Extroverted & Aggressive")){
+        $(".match").text("HTML, CSS, and Bootstrap");
+      }
+      else if(pref === "front-end" && (exp === "A moderate amount") && (personality === "Introverted & Passive" || personality === "Introverted & Aggressive")){
+        $(".match").text("JavaScript, JQuery, and React.js");
+      }
+      else if(pref === "back-end" && (exp === "None at all") && (personality === "Extroverted & Passive")){
+        $(".match").text("PHP");
+      }
+      else if(pref === "back-end" && (exp === "None at all") && (personality === "Extroverted & Aggressive")){
+        $(".match").text("C++");
+      }
+      else if(pref === "back-end" && (exp === "None at all") && (personality === "Introverted & Passive" || personality === "Introverted & Aggressive")){
+        $(".match").text("Java");
+      }
+      else if(pref === "back-end" && (exp === "A little bit") && (personality === "Extroverted & Passive")){
+        $(".match").text("PHP and C++");
+      }
+      else if(pref === "back-end" && (exp === "A little bit") && (personality === "Extroverted & Aggressive")){
+        $(".match").text("C++ and Python");
+      }
+      else if(pref === "back-end" && (exp === "A little bit") && (personality === "Introverted & Passive" || personality === "Introverted & Aggressive")){
+        $(".match").text("Java and Node.js");
+      }
+      else if(pref === "back-end" && (exp === "A moderate amount") && (personality === "Extroverted & Passive" || personality === "Extroverted & Aggressive")){
+        $(".match").text("PHP, C++, and Python");
+      }
+      else if(pref === "front-end" && (exp === "A moderate amount") && (personality === "Introverted & Passive" || personality === "Introverted & Aggressive")){
+        $(".match").text("Java, Node.js, and Ruby");
+      }
+      else{
+        $(".match").text("Doesn't fit and current chriteria");
+      }
+      $("#answer").show();
+      $("#hours-show").text(hours);
+      $("#days-show").text(days);
+      $("#output").text(resultb.toFixed()); 
     event.preventDefault();
   });
 });
